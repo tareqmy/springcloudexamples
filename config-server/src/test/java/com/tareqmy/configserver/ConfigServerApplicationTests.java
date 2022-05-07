196e51282bf2fab0e145f40fd4e3c05972eac9c0
Tareq Mohammad Yousuf <tareq.y@gmail.com>
2022-05-07 12:36:30 +0600

    config server module added

---
+++ config-server/src/test/java/com/tareqmy/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tareqmy.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
