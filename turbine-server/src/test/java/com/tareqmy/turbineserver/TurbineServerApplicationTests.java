d14801e7abd025250dcfa6dc97622a507784ed46
Tareq Mohammad Yousuf <tareq.y@gmail.com>
2022-05-08 16:43:55 +0600

    turbine server for multiple hystrix stream monitoring

---
+++ turbine-server/src/test/java/com/tareqmy/turbineserver/TurbineServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tareqmy.turbineserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurbineServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
