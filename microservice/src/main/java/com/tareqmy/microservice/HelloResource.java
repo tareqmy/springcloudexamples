f656cd96ffe5ef14a22748c7d23d2cab29c812f7
Tareq Mohammad Yousuf <tareq.y@gmail.com>
2022-05-08 15:25:45 +0600

    added hystrix circuit breaker

--- microservice/src/main/java/com/tareqmy/microservice/HelloResource.java
+++ microservice/src/main/java/com/tareqmy/microservice/HelloResource.java
@@ -1,18 +1,38 @@
 package com.tareqmy.microservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 /**
  * Created by tareqmy at 6/5/22
  */
 @RestController
-@RequestMapping("/hello")
 public class HelloResource {
 
-    @GetMapping("/")
-    public String getHello() {
-        return "Hello World!";
+    private final Random random = new Random();
+
+    @HystrixCommand(defaultFallback = "getHelloDefault")
+    @GetMapping("/hello")
+    public String getHelloWorld() {
+        return getHelloSlow();
+    }
+
+    public String getHelloSlow() {
+        int i = random.ints(500, 1100)
+            .findFirst()
+            .getAsInt();
+        try {
+            Thread.sleep(i);
+        } catch (InterruptedException e) {
+
+        }
+        return "Hello World! >>> Fast";
+    }
+
+    public String getHelloDefault() {
+        return "Hello World! >>> Default";
     }
 }
