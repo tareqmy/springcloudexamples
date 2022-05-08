c5cc5dc28c3b20f0837a5061976cc4e5939c3f8e
Tareq Mohammad Yousuf <tareq.y@gmail.com>
2022-05-08 21:27:49 +0600

    gateway server with one route to microservices

---
+++ gateway-server/src/test/java/com/tareqmy/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tareqmy.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
