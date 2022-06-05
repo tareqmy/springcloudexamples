2447310d3aeb8527b90e33f7588969cf54d545e0
Tareq Mohammad Yousuf <tareq.y@gmail.com>
2022-06-05 21:18:21 +0600

    adding caching for the microservices

--- organization-service/src/main/java/com/tareqmy/organizationservice/client/DepartmentClient.java
+++ organization-service/src/main/java/com/tareqmy/organizationservice/client/DepartmentClient.java
@@ -1,6 +1,7 @@
 package com.tareqmy.organizationservice.client;
 
 import com.tareqmy.organizationservice.model.Department;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +12,12 @@ import java.util.List;
 @FeignClient(name = "department-service")
 public interface DepartmentClient {
 
+    @Cacheable(value = "departments_cache", key = "'id'")
     @GetMapping("/organization/{organizationId}")
     List<Department> findByOrganization(@RequestHeader("Authorization") String authorizationHeader,
                                         @PathVariable("organizationId") Long organizationId);
 
+    @Cacheable(value = "departments_with_employees_cache", key = "'id'")
     @GetMapping("/organization/{organizationId}/with-employees")
     List<Department> findByOrganizationWithEmployees(@RequestHeader("Authorization") String authorizationHeader,
                                                      @PathVariable("organizationId") Long organizationId);
--- organization-service/src/main/java/com/tareqmy/organizationservice/client/EmployeeClient.java
+++ organization-service/src/main/java/com/tareqmy/organizationservice/client/EmployeeClient.java
@@ -1,6 +1,7 @@
 package com.tareqmy.organizationservice.client;
 
 import com.tareqmy.organizationservice.model.Employee;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import java.util.List;
 @FeignClient(name = "employee-service")
 public interface EmployeeClient {
 
+    @Cacheable(value = "employees_cache", key = "'id'")
     @GetMapping("/organization/{organizationId}")
     List<Employee> findByOrganization(@RequestHeader("Authorization") String authorizationHeader,
                                       @PathVariable("organizationId") Long organizationId);
